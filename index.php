84db6ba31cbd1969970cf804b70d643ea63d5492
Stjepan Mudronja <smudronja.sm@gmail.com>
2018-02-17 16:47:12 +0100

    Connect To A Database Using PDO PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+	include_once 'includes/dbh.inc.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+
+	<?php 
+	$object = new Dbh;
+	$object->connect();
+	 ?>
+	
+</body>
+</html>
